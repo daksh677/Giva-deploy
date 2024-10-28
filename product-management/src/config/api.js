32244e36cb05d49b6bc89159bf92ef3665afd4ee
dakshks677 <dakshks22@iitk.ac.in>
2024-10-28 22:50:56 +0530

    deployment 3

--- product-management/src/config/api.js
+++ product-management/src/config/api.js
@@ -1,9 +1,10 @@
 // production
-const SERVER_URL = 'https://giva-deploy-backend.onrender.com';
-const SERVER_PORT = '5000';
+// const SERVER_URL = 'https://giva-deploy-backend.onrender.com';
+// const SERVER_PORT = '5000';
 
 // local
 // const SERVER_URL = 'https://localhost';
 // const SERVER_PORT = '5000';
 
-export const API_URL = `${SERVER_URL}:${SERVER_PORT}`;
+export const API_URL = process.env.REACT_APP_API_URL || `http://localhost:5000`;
+
